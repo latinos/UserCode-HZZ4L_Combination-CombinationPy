9f62bd088e7ffb3ca00c64fbb146307fe75a0be2
namapane <>
2012-09-21 00:12:41 +0000

    fix mass point list

--- AnalysisInputs/Config.h
+++ AnalysisInputs/Config.h
@@ -1,8 +1,22 @@
 //----------> SET INPUT VARIABLES HERE
 
 // Input trees
-TString filePath7TeV = "root://lxcms02//data/Higgs/rootuplesOut/310812_ggRescaled_HighMassReweight/PRODFSR/";
-TString filePath8TeV = "root://lxcms02//data/Higgs/rootuplesOut/310812_ggRescaled_HighMassReweight/PRODFSR_8TeV/";
+
+// ICHEP inputs
+// TString filePath7TeV = "root://lxcms02//data/Higgs/rootuplesOut/240612/PRODFSR";
+// TString filePath8TeV = "root://lxcms02//data/Higgs/rootuplesOut/240612/PRODFSR_8TeV";
+
+// With LD fixes (phi1, Z1/Z2)
+// TString filePath7TeV = "root://lxcms02//data/Higgs/rootuplesOut/310812/PRODFSR";
+// TString filePath8TeV = "root://lxcms02//data/Higgs/rootuplesOut/310812/PRODFSR_8TeV";
+
+// With LD fixes (phi1, Z1/Z2), but use rescaled 7-TeV gZZ for 8 TeV
+TString filePath7TeV = "root://lxcms02//data/Higgs/rootuplesOut/310812_ggRescaled/PRODFSR";
+TString filePath8TeV = "root://lxcms02//data/Higgs/rootuplesOut/310812_ggRescaled/PRODFSR_8TeV";
+
+// Experimental
+//TString filePath7TeV = "root://lxcms02//data/Higgs/rootuplesOut/310812_ggRescaled_HighMassReweight/PRODFSR/";
+//TString filePath8TeV = "root://lxcms02//data/Higgs/rootuplesOut/310812_ggRescaled_HighMassReweight/PRODFSR_8TeV/";
 
 // Luminosity, as float and as string to be used in file names, etc.
 double lumi7TeV = 5.051;
@@ -15,9 +29,9 @@ TString lumistr8TeV = "5.261";
 TString DataRootFilePath = "../CreateDatacards/CMSdata/"; 
 //<----------
 
-const int nPoints7TeV = 23;
-int masses7TeV[nPoints7TeV]   = {120,125,130,140,150,160,170,180,200,210,220,250,300,325,350,400,425,450,475,525,550,575,600};
-double mHVal7TeV[nPoints7TeV] = {120,125,130,140,150,160,170,180,200,210,220,250,300,325,350,400,425,450,475,525,550,575,600};
+const int nPoints7TeV = 22;
+int masses7TeV[nPoints7TeV]   = {120,/*125,*/130,140,150,160,170,180,200,210,220,250,300,325,350,400,425,450,475,525,550,575,600}; //FIXME: weights for 125 are wrong.
+double mHVal7TeV[nPoints7TeV] = {120,/*125,*/130,140,150,160,170,180,200,210,220,250,300,325,350,400,425,450,475,525,550,575,600};
 
 const int nPoints8TeV = 29;
 int masses8TeV[nPoints8TeV]   = {115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,145,150,180,200,250,300,325,350,400,450,500,550,600};
@@ -29,13 +43,13 @@ double mHVal8TeV[nPoints8TeV] = {115,116,117,118,119,120,121,122,123,124,125,126
 // THIS IS TO BE USED TO ADD THE HIGH MASS POINTS
 //--------------------
 // The number and values of mass points for which you have the trees, for 7 and 8 TeV
-//const int nPoints7TeV = 29;
-//int masses7TeV[nPoints7TeV]   = {120,125,130,140,150,160,170,180,200,210,220,250,300,325,350,400,425,450,475,525,550,575,600,650,700,750,800,900,950}; //FIXME
-//double mHVal7TeV[nPoints7TeV] = {120,125,130,140,150,160,170,180,200,210,220,250,300,325,350,400,425,450,475,525,550,575,600,650,700,750,800,900,950}; //FIXME
-//
-//const int nPoints8TeV = 37;
-//int masses8TeV[nPoints8TeV]   = {115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,145,150,180,200,250,300,325,350,400,450,500,550,600,650,700,750,800,850,900,950,1000};
-//double mHVal8TeV[nPoints8TeV] = {115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,145,150,180,200,250,300,325,350,400,450,500,550,600,650,700,750,800,850,900,950,1000};
+// const int nPoints7TeV = 27;
+// int masses7TeV[nPoints7TeV]   = {120,/*125,*/130,140,150,160,170,180,200,210,220,250,300,325,350,400,425,450,475,525,550,575,600,650,700,750,/*800,*/900,950}; //FIXME: weights for 125 are wrong. There's a problem with 4mu efficiency for 800. 850 and 1000 are missing for the moment.
+// double mHVal7TeV[nPoints7TeV] = {120,/*125,*/130,140,150,160,170,180,200,210,220,250,300,325,350,400,425,450,475,525,550,575,600,650,700,750,/*800,*/900,950};
+
+// const int nPoints8TeV = 37;
+// int masses8TeV[nPoints8TeV]   = {115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,145,150,180,200,250,300,325,350,400,450,500,550,600,650,700,750,800,850,900,950,1000};
+// double mHVal8TeV[nPoints8TeV] = {115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,145,150,180,200,250,300,325,350,400,450,500,550,600,650,700,750,800,850,900,950,1000};
 
 //--------------------
 
