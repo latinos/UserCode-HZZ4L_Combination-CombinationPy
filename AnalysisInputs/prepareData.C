34354a24235bf36f0986a93a1db4d7fc5a333837
namapane <>
2012-09-28 12:25:53 +0000

    add ability to recompute MELA

--- AnalysisInputs/prepareData.C
+++ AnalysisInputs/prepareData.C
@@ -25,21 +25,37 @@
 #include <string>
 #include "TTree.h"
 #include "TText.h"
-#include "TROOT.h"
 #include "TStyle.h"
-#include "TSystem.h"
+
+#if !defined(__CINT__) || defined(__MAKECINT__)
+#include <TSystem.h>
+#include <TROOT.h>
+#include "ZZMatrixElement/MELA/interface/Mela.h"
+#endif
+
+
 
 //----------> SET INPUT VARIABLES in Config.h
 #include "Config.h"
 //<----------
 
+//--- Flags to control re-computation of KD
+bool recompute_ = false;       // Recompute KD (instead of taking it from the tree); when true, the following flags apply:
+bool usePowhegTemplate=false;  // false use analytic bg
+bool withPt_ = false;          // Include pT in KD
+bool withY_  = false;          //    "    Y  "  "
+int sqrts    = 8;              // sqrts, used only for withPt_/withY_
+
+
+Mela* myMELA;
 
 void convertTreeForDatacards(TString inFile, TString outfile);
 
-
-
 // Run all final states and sqrts in one go
 void prepareData() {
+
+  if (recompute_) myMELA = new Mela(usePowhegTemplate); // this is safely leaked
+
   gSystem->Exec("mkdir -p "+ DataRootFilePath);
   convertTreeForDatacards(filePath7TeV + "/data/HZZ4lTree_DoubleMu.root",  DataRootFilePath+"hzz4mu_"  +lumistr7TeV+".root");
   convertTreeForDatacards(filePath7TeV + "/data/HZZ4lTree_DoubleEle.root", DataRootFilePath+"hzz4e_"   +lumistr7TeV+".root");
@@ -57,9 +73,22 @@ void convertTreeForDatacards(TString inFile, TString outfile){
   treedata->Add(inFile);
 
   float mzz, pseudomela, mela;
+  float m1, m2, costheta1, costheta2, costhetastar, phi, phi1;
+  float pt4l, Y4l;
+
   treedata->SetBranchAddress("ZZMass",&mzz);
   treedata->SetBranchAddress("ZZpseudoLD",&pseudomela); 
   treedata->SetBranchAddress("ZZLD",&mela); 
+  treedata->SetBranchAddress("Z1Mass",&m1);
+  treedata->SetBranchAddress("Z2Mass",&m2);
+  treedata->SetBranchAddress("helcosthetaZ1",&costheta1);
+  treedata->SetBranchAddress("helcosthetaZ2",&costheta2);
+  treedata->SetBranchAddress("costhetastar",&costhetastar);
+  treedata->SetBranchAddress("helphi",&phi);
+  treedata->SetBranchAddress("phistarZ1",&phi1);
+  
+  treedata->SetBranchAddress("ZZPt",&pt4l);
+  treedata->SetBranchAddress("ZZRapidity",&Y4l);  
 
   TFile* newFile  = new TFile(outfile, "RECREATE");
   newFile->cd();
@@ -73,9 +102,28 @@ void convertTreeForDatacards(TString inFile, TString outfile){
   for(int iEvt=0; iEvt<treedata->GetEntries(); iEvt++){
     //    if(iEvt%5000==0) cout << "event: " << iEvt << endl;
     treedata->GetEntry(iEvt);
+
     CMS_zz4l_mass = mzz;
     pseudomelaLD = pseudomela;
     melaLD = mela;
+
+    if(recompute_){
+      float KD, psig, pbkg;
+      myMELA->computeKD(mzz,m1,m2,
+		       costhetastar,
+		       costheta1,
+		       costheta2,
+		       phi,
+		       phi1,
+		       KD,psig,pbkg,
+		       withPt_,pt4l,
+		       withY_, Y4l,
+		       sqrts);
+      
+      melaLD = KD;
+    }
+
+
     newTree->Fill();
   }
 
