1833b51dbdfb569036ad0263f9e6d9159807f59b
namapane <>
2012-10-29 11:24:33 +0000

    Additional points

--- AnalysisInputs/Config.h
+++ AnalysisInputs/Config.h
@@ -27,8 +27,14 @@
 //TString filePath8TeV = "root://lxcms02//data/Higgs/rootuplesOut/171012/PRODFSR_8TeV/";
 
 //181012, HCP unblinding
-TString filePath7TeV = "root://lxcms02//data/Higgs/rootuplesOut/181012/PRODFSR/";
-TString filePath8TeV = "root://lxcms02//data/Higgs/rootuplesOut/181012/PRODFSR_8TeV/";
+// TString filePath7TeV = "root://lxcms02//data/Higgs/rootuplesOut/191012/PRODFSR/";
+// TString filePath8TeV = "root://lxcms02//data/Higgs/rootuplesOut/191012/PRODFSR_8TeV/";
+
+//Final set, with ICHEP data/MC SFs
+TString filePath7TeV = "root://lxcms02//data/Higgs/rootuplesOut/261012_ichep/PRODFSR/";
+TString filePath8TeV = "root://lxcms02//data/Higgs/rootuplesOut/261012_ichep/PRODFSR_8TeV/";
+
+
 
 // Luminosity, as float and as string to be used in file names, etc.
 double lumi7TeV = 5.051;
@@ -55,13 +61,13 @@ TString DataRootFilePath = "../CreateDatacards/CMSdata/";
 // THIS IS TO BE USED TO ADD THE HIGH MASS POINTS
 //--------------------
 // The number and values of mass points for which you have the trees, for 7 and 8 TeV
-const int nPoints7TeV = 33;//34;
-int masses7TeV[nPoints7TeV]   = {120,124,125,126,130,140,150,160,170,180,190,200,210,220,250,275,300,325,350,400,425,450,475,525,550,575,600,650,700,750,800,900,950/*,1000*/};
-double mHVal7TeV[nPoints7TeV] = {120,124,125,126,130,140,150,160,170,180,190,200,210,220,250,275,300,325,350,400,425,450,475,525,550,575,600,650,700,750,800,900,950/*,1000*/};
+const int nPoints7TeV = 34;
+int masses7TeV[nPoints7TeV]   = {120,124,125,126,130,140,150,160,170,180,190,200,210,220,250,275,300,325,350,400,425,450,475,525,550,575,600,650,700,750,800,900,950,1000};
+double mHVal7TeV[nPoints7TeV] = {120,124,125,126,130,140,150,160,170,180,190,200,210,220,250,275,300,325,350,400,425,450,475,525,550,575,600,650,700,750,800,900,950,1000};
 
-const int nPoints8TeV = 45;//49;
-int masses8TeV[nPoints8TeV]   = {115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,135,140,145,150,160,170,180,190,200,220,250,275,300,325,350,375,400/*,425*/,450/*,475*/,500/*,525*/,550/*,575*/,600,650,700,750,800,850,900,950,1000};
-double mHVal8TeV[nPoints8TeV] = {115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,135,140,145,150,160,170,180,190,200,220,250,275,300,325,350,375,400/*,425*/,450/*,475*/,500/*,525*/,550/*,575*/,600,650,700,750,800,850,900,950,1000};
+const int nPoints8TeV = 49;
+int masses8TeV[nPoints8TeV]   = {115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,135,140,145,150,160,170,180,190,200,220,250,275,300,325,350,375,400,425,450,475,500,525,550,575,600,650,700,750,800,850,900,950,1000};
+double mHVal8TeV[nPoints8TeV] = {115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,135,140,145,150,160,170,180,190,200,220,250,275,300,325,350,375,400,425,450,475,500,525,550,575,600,650,700,750,800,850,900,950,1000};
 
 //--------------------
 
