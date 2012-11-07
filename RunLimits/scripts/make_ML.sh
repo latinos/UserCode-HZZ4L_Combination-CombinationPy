b37960e532ec8eb220d7c813e24e9b61d51ea21a
dmeister <>
2012-11-07 10:35:02 +0000

    This commit was manufactured by cvs2svn to create tag 'bonato_supermela_20121107'.
    Sprout from master 2012-11-07 10:35:01 UTC bonato 'Fix another problem affecting templates for graviMELA'
    Cherrypick from master 2012-11-01 10:31:32 UTC bonato 'small cosmetic fixes':
        RunLimits/scripts/make_ML.sh
        SignalSeparation/execute_SignalSeparationCombine.sh
    Cherrypick from HCP_Branch 2012-11-06 21:22:20 UTC mschen '*** empty log message ***':
        CreateDatacards/datacardClass.py
        CreateDatacards/systematicsClass.py
    Delete:
        CreateDatacards/CMSdata/hzz2e2mu_12.21.root
        CreateDatacards/CMSdata/hzz2e2mu_5.051.root
        CreateDatacards/CMSdata/hzz2e2mu_5.261.root
        CreateDatacards/CMSdata/hzz4e_12.21.root
        CreateDatacards/CMSdata/hzz4e_5.051.root
        CreateDatacards/CMSdata/hzz4e_5.261.root
        CreateDatacards/CMSdata/hzz4mu_12.21.root
        CreateDatacards/CMSdata/hzz4mu_5.051.root
        CreateDatacards/CMSdata/hzz4mu_5.261.root

--- RunLimits/scripts/make_ML.sh
+++ RunLimits/scripts/make_ML.sh
@@ -1,64 +1,36 @@
 #!/bin/bash
-
-POST=""; POPT="";
-if [[ "$1" == "-q" ]]; then POST=".Q"; fi
-if [[ "$1" == "-1" ]]; then POST=".1"; fi
-
-FIT="--robustFit=1"; if [[ "$1" == "-q" ]]; then FIT=""; shift; fi;
-STRICT=0; if [[ "$1" == "-s" ]]; then STRICT=1; shift; elif [[ "$1" == "-l" ]]; then STRICT=-1; shift; elif [[ "$1" == "-h" ]]; then STRICT=-0.5; shift; fi;
-STEP="0.05"; if [[ "$1" == "-t" ]]; then STEP="0.01"; shift; fi;
-MINIM=""; if [[ "$1" == "-1" ]]; then MINIM="--minimizerAlgo=Minuit --minimizerAlgoForMinos=SeqMinimizer"; shift; fi;
-EXTRA="0"; if [[ "$1" == "-x" ]]; then EXTRA=1; shift; fi;
-WHAT="ML"
-if [[ "$1" == "--TS" ]]; then WHAT="MLTS"; shift; fi;
-if [[ "$1" == "--ES" ]]; then WHAT="MLES"; shift; fi;
-if [[ "$1" == "--TB" ]]; then WHAT="MLTB"; shift; fi;
-
-RANGE="auto";
-if [[ "$1" == "-Z" ]];   then WHAT="${WHAT}Z";                shift; fi;
-if [[ "$1" == "--ZM" ]]; then WHAT="${WHAT}Z"; RANGE="input"; shift; fi;
-ZERO="--preFitValue=0.01"; 
-
-MINIM="$MINIM --stepSize=$STEP"
-
+STRICT=0; if [[ "$1" == "-s" ]]; then STRICT=1; shift; fi;
+MINIM=0; if [[ "$1" == "-1" ]]; then MINIM="--minimizerAlgo=Minuit"; shift; fi;
 if test -d $1; then MASS=$1; else echo "Usage: $0 mass [what ]"; exit 1; fi; 
-cd $MASS; shift;
-MATCH=$1;
+cd $MASS
+WHAT="ML"
+MATCH=$2;
 
-OPTIONS="$MINIM $ZERO"
+OPTIONS=" --optimizeSim=1 --minimizerStrategy=2"
+#OPTIONS=" --optimizeSim=1 $MINIM "
 if [[ "$STRICT" == 1 ]]; then
-   OPTIONS=" --minimizerStrategy=2 --minimizerTolerance=0.0001 $MINIM $ZERO" 
-elif [[ "$STRICT" == -1 ]]; then
-   OPTIONS=" --minimizerStrategy=0 --minimizerTolerance=0.1 $MINIM $ZERO" 
-elif [[ "$STRICT" == -0.5 ]]; then
-   OPTIONS=" --minimizerStrategy=0 --minimizerTolerance=0.01 $MINIM $ZERO" 
-elif [[ "$POPT" != "" ]]; then
-   OPTIONS="$MINIM $ZERO $POPT";
-else 
-   #OPTIONS="$MINIM $ZERO $FIT --X-rtd FITTER_DYN_STEP  --cminFallbackAlgo Minuit,0.001 " # this seems good at the moment
-   OPTIONS="$MINIM $ZERO $FIT --X-rtd FITTER_NEW_CROSSING_ALGO --minimizerAlgoForMinos=Minuit2 --minimizerToleranceForMinos=0.01 --X-rtd FITTER_NEVER_GIVE_UP --minimizerAlgo=Minuit2 --minimizerStrategy=1 --minimizerTolerance=0.1 --cminFallbackAlgo Minuit,0.001 -v 100" # this seems good at the moment
-#   OPTIONS="$MINIM $ZERO $FIT --X-rtd FITTER_NEW_CROSSING_ALGO --minimizerAlgoForMinos=Minuit2 --minimizerToleranceForMinos=0.01 --X-rtd FITTER_NEVER_GIVE_UP --X-rtd FITTER_BOUND --minimizerAlgo=Minuit2 --minimizerStrategy=1 --minimizerTolerance=0.001 --cminFallbackAlgo Minuit,0.001 " # this seems good at the moment
-   #OPTIONS="$MINIM $ZERO $FIT"
+   OPTIONS=" --optimizeSim=1 --minimizerStrategy=2 --minimizerTolerance=0.00001" 
 fi
-if [[ "$EXTRA" == "0" ]]; then OPTIONS="$OPTIONS --justFit"; fi;
 function run {
     WHAT=$1; shift
-    NAM=$(echo $1 | sed -e s/comb_*// -e s/.root//   | tr '[a-z]' '[A-Z]')
-    OPTS="$OPTIONS"
-    if [[ "$RANGE" == "auto" ]]; then
-        MIN=0; MAX=3; if echo "$WHAT" | grep -q "Z"; then MIN=-2; fi;
-        OPTS="$OPTS  --rMin=$MIN --rMax=$MAX";
-    fi;
+    NAM=$(echo $1 | sed -e s/comb_// -e s/.root//   | tr [a-z] [A-Z])
     if [[ "$MATCH" == "" || "$MATCH" == "$1" ]]; then
         if test -f $1; then
-             test -f ${1/.root/.log}.$WHAT$POST && rm ${1/.root/.log}.$WHAT$POST;
-             [[ "$COMBINE_NO_LOGFILES" != "1" ]] && DO_LOG="tee -a ${1/.root/.log}.$WHAT$POST" || DO_LOG="dd of=/dev/null" 
-             echo "c -M MaxLikelihoodFit $* -n ${NAM}_${WHAT}${POST} -m $MASS $OPTS "     | $DO_LOG; 
-	     combine -M MaxLikelihoodFit $* -n ${NAM}_${WHAT}${POST} -m $MASS $OPTS  2>&1 | $DO_LOG; 
+             combine -M MaxLikelihoodFit $* -n ${NAM}_${WHAT} -m $MASS $OPTIONS --out . 2>&1 | tee ${1/.root/.log.$WHAT} 
         fi;
     fi;
 }
 
 
-run $WHAT $*
+if [[ "$MATCH" == "" ]]; then
+#    run $WHAT comb_hgg.root
+#    run $WHAT comb_hww.root
+#    run $WHAT comb_htt.root
+    run $WHAT comb_hzz4l.root
+#    run $WHAT comb_hzz2l2nu.root
+#    run $WHAT comb_hzz2l2q.root
+#    run $WHAT comb.root
+else
+    run $WHAT $MATCH
+fi
 
