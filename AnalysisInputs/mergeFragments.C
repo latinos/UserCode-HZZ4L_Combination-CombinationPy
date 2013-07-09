0517512f897faaf4efa227938ab6f00c6c0bc886
chmartin <>
2013-07-09 12:36:02 +0000

    new Spin Fragments

--- AnalysisInputs/mergeFragments.C
+++ AnalysisInputs/mergeFragments.C
@@ -129,12 +129,14 @@ void mergeFragments(int channel, int sqrts, double lumi, bool dijettag) {
   append("CardFragments/sys_" + bkg_untagged, outfile2);
   append("CardFragments/hypTesting.txt", outfile);
   append("CardFragments/hypTesting.txt", outfile2);
+  append("CardFragments/spinyields_"+sig_untagged,outfile2);
   append("CardFragments/dijettagging_"+sig_untagged,outfile);
   append("CardFragments/signalEfficiency_"+ssqrts+"_"+schannel+"_ratio.txt", outfile);
   append("CardFragments/mekd_" + bkg_untagged, outfile);
   append("CardFragments/mekd_" + bkg_untagged, outfile2);
   append("CardFragments/relerr_" + bkg_untagged, outfile);
   append("CardFragments/relerr_" + bkg_untagged, outfile2);
+  append("CardFragments/spinyields_"+sig_untagged,outfile2);
 
   cout << "Wrote " << outfile << endl;
   cout << "Wrote " << outfile2 << endl;
