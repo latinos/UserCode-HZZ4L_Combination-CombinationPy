9b4bd3415bf1d249f17321cb28f23805ffc30a93
namapane <>
2013-07-12 10:15:54 +0000

    *** empty log message ***

--- AnalysisInputs/Config.h
+++ AnalysisInputs/Config.h
@@ -44,10 +44,10 @@
 //TString filePath7TeV = "root://lxcms02//data/Higgs/rootuplesOut/130120/v2/PRODFSR/";
 //TString filePath8TeV = "root://lxcms02//data/Higgs/rootuplesOut/130120/v2/PRODFSR_8TeV/";
 
+// Moriond, final set
+//TString filePath7TeV = "root://lxcms02//data/Higgs/rootuplesOut/130205/PRODFSR/";
+//TString filePath8TeV = "root://lxcms02//data/Higgs/rootuplesOut/130205/PRODFSR_8TeV/";
 // Legacy paper
-//TString filePath7TeV = "root://lxcms02//data/Higgs/rootuplesOut/130613b/PRODFSR/";
-//TString filePath8TeV = "root://lxcms02//data/Higgs/rootuplesOut/130613b/PRODFSR_8TeV/";
-
 TString filePath7TeV = "root://lxcms02//data/Higgs/rootuplesOut/130702b/PRODFSR/";	 
 TString filePath8TeV = "root://lxcms02//data/Higgs/rootuplesOut/130702b/PRODFSR_8TeV/";
 
@@ -62,9 +62,12 @@ TString lumistr8TeV = "19.79";
 // Location of output root files containing data events
 TString DataRootFilePath = "../CreateDatacards/CMSdata/"; 
 
-// THIS IS TO BE USED TO ADD THE HIGH MASS POINTS
 //--------------------
-// The number and values of mass points for which you have ggH trees, for 7 and 8 TeV
+// The number and values of mass points for which you have signal trees, for 7 and 8 TeV
+
+//--- ggH
+
+// Old powheg ggH samples
 const int nPoints7TeV = 34;
 int masses7TeV[nPoints7TeV]   = {120,124,125,126,130,140,150,160,170,180,190,200,210,220,250,275,300,325,350,400,425,450,475,525,550,575,600,650,700,750,800,900,950,1000};
 double mHVal7TeV[nPoints7TeV] = {120,124,125,126,130,140,150,160,170,180,190,200,210,220,250,275,300,325,350,400,425,450,475,525,550,575,600,650,700,750,800,900,950,1000};
@@ -73,19 +76,22 @@ const int nPoints8TeV = 49;
 int masses8TeV[nPoints8TeV]   = {115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,135,140,145,150,160,170,180,190,200,220,250,275,300,325,350,375,400,425,450,475,500,525,550,575,600,650,700,750,800,850,900,950,1000};
 double mHVal8TeV[nPoints8TeV] = {115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,135,140,145,150,160,170,180,190,200,220,250,275,300,325,350,375,400,425,450,475,500,525,550,575,600,650,700,750,800,850,900,950,1000};
 
-const int nPoints7TeV_p15 = 12;
-int masses7TeV_p15[nPoints7TeV_p15]   = {125,126,400,450,500,550,600,650,700,800,900,1000};
-double mHVal7TeV_p15[nPoints7TeV_p15] = {125,126,400,450,500,550,600,650,700,800,900,1000};
+// Full list of new powheg samples: you must use "powheg15_jhuGenV3" samples for m<=200, "powheg15" ones above. 
+const int nPoints8TeV_p15 = 28;
+int masses8TeV_p15[nPoints8TeV_p15]   = {115,122,124,125,130,135,140,145,160,170,180,185,190,225,250,275,300,350,400,450,500,550,600,650,700,800,900,1000};
+double mHVal8TeV_p15[nPoints8TeV_p15] = {115,122,124,125,130,135,140,145,160,170,180,185,190,225,250,275,300,350,400,450,500,550,600,650,700,800,900,1000};
 
-const int nPoints8TeV_p15 = 17;
-int masses8TeV_p15[nPoints8TeV_p15]   = {125,126,225,250,275,300,350,400,450,500,550,600,650,700,800,900,1000};
-double mHVal8TeV_p15[nPoints8TeV_p15] = {125,126,225,250,275,300,350,400,450,500,550,600,650,700,800,900,1000};
+//List of "powheg15" ggH samples alone (available for high mass only +125,126); just for debugging purposes
+// const int nPoints7TeV_p15 = 12;
+// int masses7TeV_p15[nPoints7TeV_p15]   = {125,126,400,450,500,550,600,650,700,800,900,1000};
+// double mHVal7TeV_p15[nPoints7TeV_p15] = {125,126,400,450,500,550,600,650,700,800,900,1000};
 
-const int nPoints8TeV_p15jhuGenV3 = 28;
-int masses8TeV_p15jhuGenV3[nPoints8TeV_p15jhuGenV3]   = {115,122,124,125,130,135,140,145,160,170,180,185,190,225,250,275,300,350,400,450,500,550,600,650,700,800,900,1000};
-double mHVal8TeV_p15jhuGenV3[nPoints8TeV_p15jhuGenV3] = {115,122,124,125,130,135,140,145,160,170,180,185,190,225,250,275,300,350,400,450,500,550,600,650,700,800,900,1000};
+// const int nPoints8TeV_p15 = 17;
+// int masses8TeV_p15[nPoints8TeV_p15]   = {125,126,225,250,275,300,350,400,450,500,550,600,650,700,800,900,1000};
+// double mHVal8TeV_p15[nPoints8TeV_p15] = {125,126,225,250,275,300,350,400,450,500,550,600,650,700,800,900,1000};
 
-//The number and values of mass points for which you have qqH trees, for 7 and 8 TeV
+
+//--- VBF
 const int nVBFPoints7TeV = 32; //FIXME: check 950 GeV sample 
 int VBFmasses7TeV[nVBFPoints7TeV]   = {115,120,125,130,140,150,160,170,180,190,200,210,220,230,250,275,300,325,350,375,400,425,450,475,500,575,600,650,700,800,900,/*950,*/1000};
 double mHVBFVal7TeV[nVBFPoints7TeV] = {115,120,125,130,140,150,160,170,180,190,200,210,220,230,250,275,300,325,350,375,400,425,450,475,500,575,600,650,700,800,900,/*950,*/1000};
@@ -94,15 +100,16 @@ const int nVBFPoints8TeV = 48;
 int VBFmasses8TeV[nVBFPoints8TeV]   = {116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,135,140,145,150,160,170,180,190,200,220,250,275,300,325,350,375,400,425,450,475,500,525,550,575,600,650,700,750,800,850,900,950,1000};
 double mHVBFVal8TeV[nVBFPoints8TeV] = {116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,135,140,145,150,160,170,180,190,200,220,250,275,300,325,350,375,400,425,450,475,500,525,550,575,600,650,700,750,800,850,900,950,1000};
 
-//The number and values of mass points for which you have VH trees, for 7 and 8 TeV
+//--- WH/ZH/ttH
 const int nVHPoints7TeV = 11;
-int VHmasses7TeV[nVHPoints7TeV]   = {110,115,120,125,126,130,140,150,160,180,200}; // FIXME 200 is missing at the moment
+int VHmasses7TeV[nVHPoints7TeV]   = {110,115,120,125,126,130,140,150,160,180,200}; // FIXME 200 is missing at the moment for ZH
 double mHVHVal7TeV[nVHPoints7TeV] = {110,115,120,125,126,130,140,150,160,180,200}; // 
 
 const int nVHPoints8TeV = 11;
 int VHmasses8TeV[nVHPoints8TeV]   = {110,115,120,125,126,130,140,150,160,180,200};
 double mHVHVal8TeV[nVHPoints8TeV] = {110,115,120,125,126,130,140,150,160,180,200};
 
+//FIXME values to be recomputed
 double filter_eff_WH_8TeV = 0.0105;
 double filter_eff_ZH_8TeV = 0.028;
 double filter_eff_ttH_8TeV = 0.0296;
